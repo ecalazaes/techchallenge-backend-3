85016212c7548c73aff14107bb83080b0bc35d1e
Erick Calazães <ecalazaes@gmail.com>
2026-03-24 14:10:51 -0300

    Javadoc config OpenApiConfig

--- scheduling-service/src/main/java/com/techchallenge/scheduling_service/config/OpenApiConfig.java
+++ scheduling-service/src/main/java/com/techchallenge/scheduling_service/config/OpenApiConfig.java
@@ -5,6 +5,20 @@ import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+
+/**
+ * Configuração global do OpenAPI (Swagger) para o serviço de agendamento.
+ * <p>
+ * Esta classe define as informações de cabeçalho da documentação interativa da API,
+ * permitindo que desenvolvedores e avaliadores visualizem e testem os endpoints
+ * diretamente pelo navegador.
+ * </p>
+ * <p>
+ * Acesse a interface em: <code>/swagger-ui.html</code>
+ * </p>
+ * @author Erick Calazães
+ *
+ */
 @Configuration
 public class OpenApiConfig {
     @Bean
