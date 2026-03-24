4c38a23470f16136fcd7d787995558c28523155d
Erick Calazães <ecalazaes@gmail.com>
2026-03-24 14:06:36 -0300

    Javadoc model UserAccount

--- scheduling-service/src/main/java/com/techchallenge/scheduling_service/model/UserAccount.java
+++ scheduling-service/src/main/java/com/techchallenge/scheduling_service/model/UserAccount.java
@@ -3,6 +3,15 @@ package com.techchallenge.scheduling_service.model;
 import jakarta.persistence.*;
 import lombok.Data;
 
+
+/**
+ * Entidade que representa as contas de usuário e suas credenciais de acesso no sistema.
+ * <p>
+ * Gerencia a autenticação e a autorização baseada em perfis (roles) para garantir
+ * que cada usuário acesse apenas as funcionalidades permitidas.
+ * </p>
+ * @author Erick Calazães
+ *  */
 @Entity
 @Table(name = "tb_users")
 @Data
@@ -18,6 +27,17 @@ public class UserAccount {
     @Column(nullable = false)
     private String password;
 
+    /**
+     * Define o nível de acesso e permissões do usuário no sistema.
+     * <p>
+     * Os valores aceitos são:
+     * <ul>
+     * <li><b>ROLE_MEDICO</b>: Acesso a agendas e prontuários.</li>
+     * <li><b>ROLE_ENFERMEIRO</b>: Acesso a triagens e consultas básicas.</li>
+     * <li><b>ROLE_PACIENTE</b>: Acesso a agendamentos próprios e histórico pessoal.</li>
+     * </ul>
+     * </p>
+     */
     @Column(nullable = false)
-    private String role; // Deve ser: ROLE_MEDICO, ROLE_ENFERMEIRO ou ROLE_PACIENTE
+    private String role;
 }
